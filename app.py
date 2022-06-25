1c72396569a2ce99a6b30586e8d26e8530eea82f
Wei-Lun Lin <weilun9539ny@gmail.com>
2022-06-26 00:16:47 +0800

    Change the message

--- app.py
+++ app.py
@@ -38,9 +38,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.message.text == "阿彌陀佛狒狒":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="蛤?")
+        )
 
 
 if __name__ == "__main__":
